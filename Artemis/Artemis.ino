4e759a1cf610f769f6854ea02c9a9babbc43448d
Bob <e4mafia@gmail.com>
2018-10-18 11:52:48 -0400

    Initial Commit

---
+++ Artemis/Artemis.ino
@@ -0,0 +1,66 @@
+//
+// Artemis
+//
+// Description of the project
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+//
+// Author 		Bob Smith
+// 				Bob Smith
+//
+// Date			10/18/18 11:52 AM
+// Version		<#version#>
+//
+// Copyright	© Bob Smith, 2018
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+// !!! Help: http://bit.ly/2AdU7cu
+#if defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.8 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+// !!! Help: http://bit.ly/2l0ZhTa
+
+
+// Utilities
+
+
+// Functions
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
---
+++ Artemis/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  Artemis
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project Artemis
+  Created by Bob Smith on 10/18/18
+  Copyright © 2018 Bob Smith
+  Licence <#licence#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2017
+  All rights reserved
+  http://embedXcode.weebly.com
+
